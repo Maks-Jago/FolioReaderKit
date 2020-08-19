98243478c6871a07ac8a1db77363396892f5fd63
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2020-08-19 13:00:57 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,14 +4,30 @@
 import PackageDescription
 
 let package = Package(
-	name: "FolioReaderKit",
-	dependencies: [
+    name: "FolioReaderKit",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "FolioReaderKit",
+            targets: ["FolioReaderKit"]),
+    ],
+    dependencies: [
         .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.1.5"),
         .package(url: "https://github.com/cxa/MenuItemKit.git", from: "4.0.1"),
-        .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", from: "0.6"),
+        .package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", .branch("master")),
         .package(url: "https://github.com/tadija/AEXML.git", from: "4.2.2"),
         .package(url: "https://github.com/ArtSabintsev/FontBlaster.git", from: "5.1.1"),
         .package(url: "https://github.com/fantim/JSQWebViewController.git", from: "6.1.1"),
         .package(url: "https://github.com/realm/realm-cocoa.git", from: "3.17.3"),
-	]
+    ],
+    targets: [
+        .target(
+            name: "FolioReaderKit",
+            dependencies: [],
+            exclude: []
+        ),
+    ]
 )
