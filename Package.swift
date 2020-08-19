77dcfe139546e319873190ea52e1311d88005b56
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2020-08-19 16:55:13 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
         .package(name: "ZipArchive", url: "https://github.com/Maks-Jago/ZipArchive.git", .branch("master")),
         .package(name: "MenuItemKit", url: "https://github.com/cxa/MenuItemKit.git", .exact("4.0.1")),
         .package(name: "ZFDragableModalTransition", url: "https://github.com/Maks-Jago/ZFDragableModalTransition.git", .branch("master")),
-        .package(name: "AEXML", url: "https://github.com/tadija/AEXML.git", .exact("4.2.2")),
+        .package(name: "AEXML", url: "https://github.com/tadija/AEXML.git", from: "4.2.2"),
         .package(name: "FontBlaster", url: "https://github.com/ArtSabintsev/FontBlaster.git", .exact("5.1.1")),
-        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", .exact("3.17.3"))
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", .exact("3.12.0"))
     ],
     targets: [
         .target(
